c3409fc6957d4c715b14e854a37821aaf2cca740
mahdi <mmf1370py@gmail.com>
2022-09-02 21:27:22 -0700

    second commit

--- config/settings.py
+++ config/settings.py
@@ -17,12 +17,13 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env()
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env.read_env('.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
