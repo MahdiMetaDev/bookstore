d2822151442bded08be09280de7dd625424d9c47
mahdi <mmf1370py@gmail.com>
2022-09-04 05:34:31 -0700

    add Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
