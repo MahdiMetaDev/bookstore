18a390fff5dc6efb16a2347a30efacc75125a266
mahdi <mmf1370py@gmail.com>
2022-09-04 08:40:45 -0700

    add BookCreateView

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.BookListView.as_view(), name='books_list'),
     path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('create/', views.BookCreateView.as_view(), name='book_create'),
 ]
--- books/views.py
+++ books/views.py
@@ -13,3 +13,10 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_create.html'
+    
