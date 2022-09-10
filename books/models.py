8962bf025da853e6678c85fa4cc2e5f4dd46f0eb
mahdi <mmf1370py@gmail.com>
2022-09-10 04:55:53 -0700

    add ImageField to Book model

--- books/models.py
+++ books/models.py
@@ -8,6 +8,7 @@ class Book(models.Model):
     description = models.TextField()
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
         return self.title
--- config/settings.py
+++ config/settings.py
@@ -160,3 +160,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+# Media files config (new)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
