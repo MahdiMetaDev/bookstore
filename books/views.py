bfbc9b2123b0f2b13553add8abc0e7fbb90c2293
mahdi <mmf1370py@gmail.com>
2022-09-10 09:19:05 -0700

    add pagination to BookListView

--- books/views.py
+++ books/views.py
@@ -8,6 +8,7 @@ class BookListView(generic.ListView):
     model = Book
     template_name = 'books/books_list.html'
     context_object_name = 'books'
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
