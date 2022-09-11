cabe208bc8815e8a3b2566f7c2ae37503b1b16ae
mahdi <mmf1370py@gmail.com>
2022-09-11 13:21:09 -0700

    register Comment model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Comment
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'datetime_created', )
 
 admin.site.register(Book)
