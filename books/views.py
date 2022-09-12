7d608f4b68bf7189c6b0cdd90e583f4b3abefc04
mahdi <mmf1370py@gmail.com>
2022-09-12 04:05:01 -0700

    change DetailView class to detail_view function and make comments validation

--- books/views.py
+++ books/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.urls import reverse_lazy
 
-from .models import Book
+from .models import Book, Comment
+from .forms import CommentForm
 
 class BookListView(generic.ListView):
     model = Book
@@ -16,10 +17,32 @@ class BookListView(generic.ListView):
         return context
         
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
-    context_object_name = 'book'
+# class BookDetailView(generic.DetailView):
+#     model = Book
+#     template_name = 'books/book_detail.html'
+#     context_object_name = 'book'
+
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    book_comments = book.comments.all()
+    
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'books/book_detail.html', context={
+        'book': book,
+        'comments': book_comments,
+        'comment_form': comment_form,
+    })
 
 
 class BookCreateView(generic.CreateView):
