e44f3c19c6fa8bcbabcc7f44c3285aa1e3ca26f7
mahdi <mmf1370py@gmail.com>
2022-09-12 14:15:14 -0700

    implement profile,signals,forms feature

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,6 @@
-from email.policy import default
 from django.db import models
 from django.contrib.auth import get_user_model
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -11,3 +11,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 100 or img.width > 100:
+            new_img = (100, 100)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
+
+from .forms import UpdateUserForm, UpdateProfileForm
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -8,5 +12,17 @@ class HomePageView(TemplateView):
 
 @login_required
 def profile(request):
-    return render(request, 'pages/profile.html')
-    
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users-profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'pages/profile.html', {'user_form': user_form, 'profile_form': profile_form})
